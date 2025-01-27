4c9732e09bd32c5ad2ebf5de1dd3b765e112da9e
Pooja <poojanb14@gmail.com>
2025-01-27 12:03:56 +0530

    19th Jan

--- src/IncrementDecrement/CLI.java
+++ src/IncrementDecrement/CLI.java
@@ -3,12 +3,11 @@ package IncrementDecrement;
 public class CLI {
     public static void main(String[] args) {
         String name = args[0];
-        String st_age=args[0];
-        int age=Integer.parseInt(st_age);
-        String st_salary=args[0];
-        int salary=Integer.parseInt(st_salary);
+        String st_age=args[1];
+        String st_salary=args[2];
+
         System.out.println(name);
-        System.out.println(age);
-        System.out.println(salary);
+        System.out.println(st_age);
+        System.out.println(st_salary);
     }
 }
